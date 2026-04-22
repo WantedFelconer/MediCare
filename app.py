2ff7c466ba5ae2e97d508c0fea69d0f10d0f2a15
wantedfelconer <emon62224@gmail.com>
2026-04-22 19:08:23 +0600

    added routes

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
 from flask import Flask, render_template
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def home():
     return render_template('home.html')
 
-if __name__ == '__main__':
+@app.route('/login', methods = ['POST','GET'])
+def login():
+    return render_template('login.html')
+
+@app.route('/register',methods=['POST','GET'])
+def register():
+    return render_template('register.html')
+
+
+
+if __name__ == "__main__":
     app.run(debug=True)
